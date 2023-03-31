9f94d73f4dda50e8aa37ed3689f42b076bfe5198
JMangoting <justianprayeslimangoting@gmail.com>
2023-03-31 09:51:00 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ var urlsToCache = [
     '/contact.html',
     '/portfolio-example01.html',
     '/style.css',
-    './js/app.js',
+    // '/js/app.js',
     '/images/about-header.jpg',
     '/images/contact-image.jpg',
     '/images/example-blog01.jpg',
@@ -18,6 +18,10 @@ var urlsToCache = [
     '/images/example-blog06.jpg',
     '/images/example-blog07.jpg',
     '/images/example-work01.jpg',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://code.getmdl.io/1.3.0/material.grey-pink.min.css',
+    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en',
+    'https://code.getmdl.io/1.3.0/material.min.js'
 ];
 
 // Install service worker
