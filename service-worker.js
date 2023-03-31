f95c6b29260662db3f2d62967f7a8134445b3c24
JMangoting <120364227+JMangoting@users.noreply.github.com>
2023-03-31 08:24:35 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+var CACHE_NAME = 'my-portfolio-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/materialize.min.css',
+  '/css/style.css',
+  '/js/materialize.min.js',
+  '/js/init.js',
+  '/images/background.jpg',
+  '/images/profile.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone request to make sure it can be consumed twice.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone response to make sure it can be consumed twice.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
